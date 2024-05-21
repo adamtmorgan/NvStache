9f28cb990b2d766543ab4e04b03b2549cdb881c0
Adam Morgan <38927602+adamtmorgan@users.noreply.github.com>
2024-05-20 23:17:13 -0400

    added auto closing tags

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,12 +1,35 @@
 return {
-    -- Auto adds closing brackets, braces, etc.
-    {
-        'windwp/nvim-autopairs',
-        event = "InsertEnter",
-        config = true
-        -- use opts = {} for passing setup options
-        -- this is equalent to setup({}) function
-    },
+	-- Auto adds closing brackets, braces, etc.
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+		-- use opts = {} for passing setup options
+		-- this is equalent to setup({}) function
+	},
+
+	-- Adds auto closing and renaming tags
+	{
+		"windwp/nvim-ts-autotag",
+		config = function()
+			require("nvim-ts-autotag").setup({
+				opts = {
+					-- Defaults
+					enable_close = true, -- Auto close tags
+					enable_rename = true, -- Auto rename pairs of tags
+					enable_close_on_slash = false, -- Auto close on trailing </
+				},
+				-- Also override individual filetype configs, these take priority.
+				-- Empty by default, useful if one of the "opts" global settings
+				-- doesn't work well in a specific filetype
+				--per_filetype = {
+				--	["html"] = {
+				--		enable_close = false,
+				--	},
+				--},
+			})
+		end,
+	},
 
 	-- Adds line guides to indents
 	{
--- lua/plugins/tresitter.lua
+++ lua/plugins/tresitter.lua
@@ -1,22 +1,33 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	--dependencies = { "windwp/nvim-ts-autotag" },
 	build = ":TSUpdate",
 	config = function()
 		local treeSitterConfig = require("nvim-treesitter.configs")
 		treeSitterConfig.setup({
 			ensure_installed = {
 				"lua",
+				"bash",
+				"jsdoc",
 				"javascript",
 				"typescript",
+				"python",
+				"tsx",
 				"html",
 				"css",
+				"scss",
 				"rust",
 				"toml",
+				"yaml",
 				"ron",
+				"vue",
+				"sql",
 			},
 			--sync_install = false,
 			highlight = { enable = true },
 			indent = { enable = true },
+			--autopairs = { enable = true },
+			--autotag = { enable = true },
 		})
 	end,
 }
