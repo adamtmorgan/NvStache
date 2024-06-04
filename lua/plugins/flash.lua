58c70f31fed099674068274fa3cb8c3ed3c3e878
Adam Morgan <adam.morgan@gocrisp.com>
2024-06-04 15:23:50 -0400

    slighly altered flash config

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -2,9 +2,13 @@ return {
 	"folke/flash.nvim",
 	event = "VeryLazy",
 	---@type Flash.Config
-	opts = {},
+	opts = {
+		label = {
+			uppercase = true,
+		},
+	},
   -- stylua: ignore
-  keys = {
+    keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
