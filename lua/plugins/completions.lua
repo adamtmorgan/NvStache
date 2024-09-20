0ea48e1d03f1741c5eee0851fe3fda00942ddce6
Adam Morgan <adam.morgan@gocrisp.com>
2024-09-19 21:53:02 -0400

    changed hint icon to lightbulb

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -86,7 +86,7 @@ return {
 				},
 				floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
 				hint_enable = true, -- virtual hint enable
-				hint_prefix = "󱗞 ", -- Panda for parameter hint
+				hint_prefix = "󰛨 ", -- Panda for parameter hint
 				hi_parameter = "Search", -- Highlight for current parameter
 			})
 		end,
