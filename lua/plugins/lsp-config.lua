3fd4ac5948a1c17b24c248209de8c1b009c74f41
Adam Morgan <adam.morgan@gocrisp.com>
2024-10-08 17:29:14 -0400

    Added arrow.nivm plugin

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -290,7 +290,7 @@ return {
 			-- keybindings
 			local map = vim.keymap.set
 
-			map("n", ";", ":", { desc = "CMD enter command mode" })
+			-- map("n", ";", ":", { desc = "CMD enter command mode" })
 			map("i", "jk", "<ESC>")
 
 			-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
