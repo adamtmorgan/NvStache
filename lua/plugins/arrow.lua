62a11ca907b1c98f636817e4ed04d6462ab0310a
Adam Morgan <adam.morgan@gocrisp.com>
2024-10-24 09:37:19 -0400

    updated Arrow.nvim to hide help options

--- lua/plugins/arrow.lua
+++ lua/plugins/arrow.lua
@@ -5,5 +5,6 @@ return {
 		leader_key = ";", -- Recommended to be a single key
 		buffer_leader_key = "m", -- Per Buffer Mappings
 		always_show_path = true,
+		hide_handbook = true, -- If false, it will show help options
 	},
 }
