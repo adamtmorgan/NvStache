d8b9617c581b1a06e098c5a8dab06e17f6b29c6c
Adam Morgan <38927602+adamtmorgan@users.noreply.github.com>
2024-12-23 19:08:37 -0600

    updated lua lsp config to reference vim runtime and lazy packages

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -132,6 +132,28 @@ return {
 
 			lspconfig.lua_ls.setup({
 				capabilities = capabilities,
+				settings = {
+					Lua = {
+						runtime = {
+							-- Tell the language server which version of Lua you're using
+							-- (most likely LuaJIT in the case of Neovim)
+							version = "LuaJIT",
+						},
+						-- Make the server aware of Neovim runtime files
+						workspace = {
+							checkThirdParty = false,
+							library = {
+								vim.env.VIMRUNTIME,
+								-- Depending on the usage, you might want to add additional paths here.
+								vim.fn.expand("$VIMRUNTIME/lua"),
+								vim.fn.stdpath("config") .. "/lua",
+								vim.fn.stdpath("data") .. "/lazy", -- Lazy plugin directory
+							},
+							-- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+							-- library = vim.api.nvim_get_runtime_file("", true)
+						},
+					},
+				},
 			})
 
 			-- No longer needed since using typescript-tools.nvim.
