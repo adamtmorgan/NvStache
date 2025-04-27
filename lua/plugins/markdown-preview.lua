5171337b041b44b264c00d9c06ebdcc49480b426
Adam Morgan <38927602+adamtmorgan@users.noreply.github.com>
2025-04-27 01:44:34 -0400

    changed markdown rendering to only apply to avante
    I didn't like how markdown was rendering in hover

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,9 +1,22 @@
 return {
-	-- install without yarn or npm
-	"iamcco/markdown-preview.nvim",
-	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	ft = { "markdown" },
-	build = function()
-		vim.fn["mkdp#util#install"]()
-	end,
+	{
+		-- install without yarn or npm
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		opts = {
+			file_types = { "Avante" },
+			code = {
+				style = "normal",
+				-- disable_background = true,
+			}
+		},
+		ft = { "markdown", "Avante" },
+	},
 }
