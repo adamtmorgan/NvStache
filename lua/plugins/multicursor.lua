c4c190ed988f133460362fc91caff194e44f26c9
Adam Morgan <38927602+adamtmorgan@users.noreply.github.com>
2025-12-30 13:48:47 -0500

    added hack that fixes cmp issues after exiting multicursor

--- lua/plugins/multicursor.lua
+++ lua/plugins/multicursor.lua
@@ -16,5 +16,24 @@ return {
             ":call vm#commands#add_cursor_down(0, v:count1)<cr>",
             { noremap = true, silent = true }
         )
+
+        -- NOTE: Hack that fixes bug that prevents blink cmp accept from working
+        -- after exiting VM. Might be able to remove if the fix is merged later.
+        -- https://github.com/mg979/vim-visual-multi/issues/291#issuecomment-3067070036
+        -- https://github.com/mg979/vim-visual-multi/pull/297 - supposedly fixes the issue.
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "visual_multi_exit",
+            callback = function()
+                local cmp = require("blink.cmp")
+                vim.keymap.set("i", "<CR>", function()
+                    if cmp.is_visible() then
+                        cmp.accept()
+                        return ""
+                    else
+                        return vim.api.nvim_replace_termcodes("<CR>", true, false, true)
+                    end
+                end, { expr = true, silent = true })
+            end,
+        })
     end,
 }
