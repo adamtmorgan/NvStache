27804a09e128ceea0617d08daf009a3964cf123d
Adam Morgan <adam.morgan@gocrisp.com>
2026-03-13 12:43:19 -0400

    removed markdown rendering that was polluting hover and completions

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,12 +1,12 @@
 return {
     -- Inline markdown previews
-    {
-        "MeanderingProgrammer/render-markdown.nvim",
-        dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
-        ---@module 'render-markdown'
-        ---@type render.md.UserConfig
-        opts = {},
-    },
+    -- {
+    --     "MeanderingProgrammer/render-markdown.nvim",
+    --     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+    --     ---@module 'render-markdown'
+    --     ---@type render.md.UserConfig
+    --     opts = {},
+    -- },
     -- For more elaborate markdown previewing in browser
     -- Watching https://github.com/selimacerbas/markdown-preview.nvim closely as a possible alternative since the
     -- below plugin is no longer maintained.
