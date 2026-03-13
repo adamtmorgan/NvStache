16bb47ef7790738f6401c7f97227268bd78d3e5f
Adam Morgan <adam.morgan@gocrisp.com>
2026-03-13 12:29:09 -0400

    added in-terminal markdown rendering

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,4 +1,15 @@
 return {
+    -- Inline markdown previews
+    {
+        "MeanderingProgrammer/render-markdown.nvim",
+        dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {},
+    },
+    -- For more elaborate markdown previewing in browser
+    -- Watching https://github.com/selimacerbas/markdown-preview.nvim closely as a possible alternative since the
+    -- below plugin is no longer maintained.
     {
         -- install without yarn or npm
         "iamcco/markdown-preview.nvim",
